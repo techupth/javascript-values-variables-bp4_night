ed06a0114466e2871cc94da3d28c5fd73a28c66f
pichaya kumdee <otakusas@hotmail.com>
2024-08-02 23:23:41 +0700

    updat:Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -8,6 +8,6 @@ const totalPrice = 15000
 const AllRestaurants = 1500
 //Exercise #2: Variable Reassignment
 //Start coding here
-address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
 totalPrice = 5000
 AllRestaurants = 2500
